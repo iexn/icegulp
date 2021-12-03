796e96360744510654ee59cdc5fd8cb2c7078a5f
iexn <iexn@qq.com>
2021-12-03 09:31:58 +0800

    2.3.10:修复打印提示时间格式

--- register/log.js
+++ register/log.js
@@ -2,7 +2,7 @@ const Log = function () {};
 const _this = Log.prototype;
 
 const full = num => num < 10 ? ('0' + num) : ('' + num);
-const time = () => (nd => `${nd.getHours()}:${nd.getMinutes()}:${nd.getSeconds()}`)(new Date);
+const time = () => (nd => `${full(nd.getHours())}:${full(nd.getMinutes())}:${full(nd.getSeconds())}`)(new Date);
 
 _this.print = function () {
     console.log('\x1B[35m['+ time() +']\x1B[0m', '\x1B[36mIceGulp\x1B[0m', '-', ...arguments);
